b88b30641f1131d3430d78fcabdfcc4cb77096a1
jos-cabrera <joseacabrera@protonmail.com>
2024-09-21 21:33:36 -0300

    add compatibility with other browsers

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -2,125 +2,131 @@ import React, { useState } from "react";
 import "./Settings.css";
 
 function Settings({
-	focusTime,
-	breakTime,
-	longBreakTime,
-	autoStartFocus,
-	autoStartBreaks,
-	pomodorosUntilLongBreak,
-	setFocusTime,
-	setBreakTime,
-	setLongBreakTime,
-	setAutoStartFocus,
-	setAutoStartBreaks,
-	setPomodorosUntilLongBreak,
-	currentStage,
-	setShowSettings,
-	updateStage,
+  focusTime,
+  breakTime,
+  longBreakTime,
+  autoStartFocus,
+  autoStartBreaks,
+  pomodorosUntilLongBreak,
+  setFocusTime,
+  setBreakTime,
+  setLongBreakTime,
+  setAutoStartFocus,
+  setAutoStartBreaks,
+  setPomodorosUntilLongBreak,
+  currentStage,
+  setShowSettings,
+  updateStage,
 }) {
-	// Create local state for the form inputs
-	const [formData, setFormData] = useState({
-		focusTime,
-		breakTime,
-		longBreakTime,
-		autoStartFocus,
-		autoStartBreaks,
-		pomodorosUntilLongBreak,
-	});
+  const [formData, setFormData] = useState({
+    focusTime,
+    breakTime,
+    longBreakTime,
+    autoStartFocus,
+    autoStartBreaks,
+    pomodorosUntilLongBreak,
+  });
 
-	// Handle input change
-	const handleChange = (event) => {
-		const { name, value, type, checked } = event.target;
-		setFormData((prevData) => ({
-			...prevData,
-			[name]: type === "checkbox" ? checked : value,
-		}));
-	};
+  const handleChange = (event) => {
+    const { name, type, checked } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : event.target.value,
+    }));
+  };
 
-	// Handle button click
-	const handleClick = (event) => {
-		event.preventDefault(); // Prevent form submission
+  const handleClick = (event) => {
+    event.preventDefault();
 
-		if (formData.focusTime < 1 || formData.focusTime > 1440) return;
-		if (formData.breakTime < 1 || formData.breakTime > 1440) return;
-		if (formData.longBreakTime < 1 || formData.longBreakTime > 1440) return;
-		if (
-			formData.pomodorosUntilLongBreak < 1 ||
-			formData.pomodorosUntilLongBreak > 1440
-		)
-			return;
+    if (formData.focusTime < 1 || formData.focusTime > 1440) return;
+    if (formData.breakTime < 1 || formData.breakTime > 1440) return;
+    if (formData.longBreakTime < 1 || formData.longBreakTime > 1440) return;
+    if (
+      formData.pomodorosUntilLongBreak < 1 ||
+      formData.pomodorosUntilLongBreak > 1440
+    )
+      return;
 
-		// Update the parent state with the new values
-		setFocusTime(formData.focusTime);
-		setBreakTime(formData.breakTime);
-		setLongBreakTime(formData.longBreakTime);
-		setAutoStartFocus(formData.autoStartFocus);
-		setAutoStartBreaks(formData.autoStartBreaks);
-		setPomodorosUntilLongBreak(formData.pomodorosUntilLongBreak);
-		updateStage(currentStage);
-		setShowSettings(false);
-	};
+    setFocusTime(formData.focusTime);
+    setBreakTime(formData.breakTime);
+    setLongBreakTime(formData.longBreakTime);
+    setAutoStartFocus(formData.autoStartFocus);
+    setAutoStartBreaks(formData.autoStartBreaks);
+    setPomodorosUntilLongBreak(formData.pomodorosUntilLongBreak);
+    updateStage(currentStage);
+    setShowSettings(false);
+  };
 
-	// Create input components with the current state as value
-	const numericSettingFields = [
-		"focusTime",
-		"breakTime",
-		"longBreakTime",
-		"pomodorosUntilLongBreak",
-	];
+  const numericSettingFields = [
+    "focusTime",
+    "breakTime",
+    "longBreakTime",
+    "pomodorosUntilLongBreak",
+  ];
 
-	const checkSettingFields = ["autoStartFocus", "autoStartBreaks"];
+  const checkSettingFields = ["autoStartFocus", "autoStartBreaks"];
 
-	const transformCamelCase = (str) => {
-		const words = str
-			.replace(/([a-z])([A-Z])/g, "$1 $2")
-			.split(" ")
-			.map((word) => word.toLowerCase());
-		words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
-		return words.join(" ");
-	};
+  const transformCamelCase = (str) => {
+    const words = str
+      .replace(/([a-z])([A-Z])/g, "$1 $2")
+      .split(" ")
+      .map((word) => word.toLowerCase());
+    words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+    return words.join(" ");
+  };
 
-	const numericSettingFieldsComponents = numericSettingFields.map((field) => (
-		<div className="input-field" key={field}>
-			<label htmlFor={field}>{transformCamelCase(field)}</label>
-			<input
-				min={1}
-				max={1440}
-				type="number"
-				name={field}
-				id={field}
-				value={formData[field]}
-				onChange={handleChange}
-			/>
-		</div>
-	));
+  const numericSettingFieldsComponents = numericSettingFields.map((field) => (
+    <div className="input-field" key={field}>
+      <label htmlFor={field}>{transformCamelCase(field)}</label>
+      <input
+        min={1}
+        max={1440}
+        type="number"
+        name={field}
+        id={field}
+        value={formData[field]}
+        onChange={handleChange}
+      />
+    </div>
+  ));
 
-	const checkSettingFieldsComponents = checkSettingFields.map((field) => (
-		<div className="input-field" key={field}>
-			<label htmlFor={field}>{transformCamelCase(field)}</label>
-			<input
-				type="checkbox"
-				name={field}
-				id={field}
-				checked={formData[field]}
-				onChange={handleChange}
-			/>
-		</div>
-	));
+  const checkSettingFieldsComponents = checkSettingFields.map((field) => (
+    <div className="input-field" key={field}>
+      <label htmlFor={field}>{transformCamelCase(field)}</label>
+      <input
+        type="checkbox"
+        name={field}
+        id={field}
+        checked={formData[field]}
+        onChange={handleChange}
+      />
+      <span
+        onClick={() =>
+          handleChange({
+            target: {
+              name: field,
+              type: "checkbox",
+              checked: !formData[field],
+            },
+          })
+        }
+      />
+    </div>
+  ));
 
-	return (
-		<>
-			<div className="settings-background"></div>
-			<form id="settings">
-				<h2 className="title settings-title">Settings</h2>
-				{numericSettingFieldsComponents}
-				{checkSettingFieldsComponents}
-				<button className="save-settings" onClick={handleClick}>
-					Save Settings
-				</button>
-			</form>
-		</>
-	);
+  return (
+    <>
+      <div className="settings-background"></div>
+      <form id="settings">
+        <h2 className="title settings-title">Settings</h2>
+        {numericSettingFieldsComponents}
+        {checkSettingFieldsComponents}
+        <button className="save-settings" onClick={handleClick}>
+          Save Settings
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default Settings;
